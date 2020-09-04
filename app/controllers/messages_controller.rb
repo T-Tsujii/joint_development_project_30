e5260ea301cbe18d05ea39d5b698df649107d701
shin <greeeen999@yahoo.co.jp>
2020-09-04 17:43:09 +0900

    新規投稿機能 new, create を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,13 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
   end
+
+  def create
+    Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :age)
+  end
 end
